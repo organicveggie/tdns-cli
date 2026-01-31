f76dcfc25d30e34884dcb84c0848a8bd61806297
Sean Laurent <organicveggie@gmail.com>
2026-01-31 09:24:03 -0800

    Split zone commands into separate files

--- tdns/src/zone.rs
+++ tdns/src/zone.rs
@@ -1,5 +1,6 @@
-use clap::ValueEnum;
+use clap::{Subcommand, ValueEnum};
 use reqwest::Client;
+use std::error::Error;
 use tabled::builder::Builder;
 use tabled::settings::Panel;
 
@@ -25,6 +26,81 @@ pub enum ZoneError {
         source: serde_json::Error,
     },
 }
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    #[command(about = "Disables an authoritative zone")]
+    Disable,
+    #[command(about = "Enables an authoritative zone")]
+    Enable,
+    #[command(about = "List records in a zone")]
+    List {
+        #[arg(help = "Optional domain name to list. Skip to list all records.")]
+        domain: Option<String>,
+        #[arg(
+            value_enum,
+            short = 'd',
+            long = "detail",
+            help = "Level of detail to include when printing zone records",
+            default_value_t = ZoneRecordDetailLevel::Summary,
+        )]
+        detail: ZoneRecordDetailLevel,
+        #[arg(
+            value_enum,
+            long = "table_style",
+            help = "Table style to use when printing zone records",
+            default_value_t = TableStyles::Ascii,
+        )]
+        table_style: TableStyles,
+    },
+}
+
+impl Command {
+    pub async fn run(&self, config_file_name: &str, zone: String) -> Result<(), ZoneError> {
+        match self {
+            Command::Disable => {
+                println!("disable {:?}", zone);
+            }
+            Command::Enable => {
+                println!("enable {:?}", zone);
+            }
+            Command::List {
+                domain,
+                detail,
+                table_style,
+            } => {
+                if let Some(domain_name) = domain {
+                    println!("list records for {:?} in {:?}", domain_name, zone);
+                } else {
+                    println!("list records for {:?}", zone);
+                }
+                let cmd = match GetRecordsCmd::create(
+                    config_file_name,
+                    zone.clone(),
+                    domain.clone(),
+                    detail.clone(),
+                    table_style.clone(),
+                ) {
+                    Ok(cmd) => cmd,
+                    Err(error) => panic!("failed to list records for {}: {}", zone, error),
+                };
+                match cmd.execute().await {
+                    Ok(()) => {}
+                    Err(error) => {
+                        eprintln!("Error: {}", error);
+                        let mut source: Option<&(dyn Error + 'static)> = error.source();
+                        while let Some(cause) = source {
+                            eprintln!("Caused by: {}", cause);
+                            source = cause.source();
+                        }
+                    }
+                }
+            }
+        }
+        return Ok(());
+    }
+}
+
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum)]
 pub enum ZoneRecordDetailLevel {
     Detailed,
