40281f95b45ff834b76bf198b0368123fcc77d25
Sean Laurent <organicveggie@gmail.com>
2026-02-11 17:23:56 -0800

    Refactor Add record to simplify testability

--- tdns/src/zone/add.rs
+++ tdns/src/zone/add.rs
@@ -115,8 +115,6 @@ impl RecordTypeCommand {
         ttl: Option<u32>,
         expiry_ttl: Option<u32>,
     ) -> Result<(), errors::TdnsError> {
-        let domain_name = make_domain_name(&domain, &zone);
-
         let cfg = match app_config.config_manager.read_config_file(config_file_name) {
             Ok(c) => c,
             Err(error) => {
@@ -124,63 +122,12 @@ impl RecordTypeCommand {
             }
         };
 
+        let domain_name = make_domain_name(&domain, &zone);
+        let query =
+            self.make_query_params(&cfg, &domain_name, &zone, overwrite, comments, ttl, expiry_ttl);
+
         let host = cfg.get_host();
-        let mut qb = QueryBuilder::new()
-            .add_param("token", &cfg.get_token())
-            .add_param("domain", &domain_name)
-            .add_param("zone", &zone)
-            .add_param("type", &self.to_string());
-
-        if overwrite {
-            qb = qb.add_param("overwrite", "true");
-        }
-        if let Some(comments) = comments {
-            qb = qb.add_param("comments", &comments);
-        }
-        if let Some(ttl) = ttl {
-            qb = qb.add_param("ttl", &ttl.to_string());
-        }
-        if let Some(expiry_ttl) = expiry_ttl {
-            qb = qb.add_param("expiryTtl", &expiry_ttl.to_string());
-        }
-
-        match self {
-            RecordTypeCommand::A { address, ptr, ptr_zone, update_svcb_hints } => {
-                let addr_qb = make_address_params(address, *ptr, *ptr_zone, *update_svcb_hints);
-                qb = qb.merge(addr_qb);
-            }
-            RecordTypeCommand::AAAA { address, ptr, ptr_zone, update_svcb_hints } => {
-                let addr_qb = make_address_params(address, *ptr, *ptr_zone, *update_svcb_hints);
-                qb = qb.merge(addr_qb);
-            }
-            RecordTypeCommand::CNAME { cname } => {
-                qb = qb.add_param("cname", cname);
-            }
-            RecordTypeCommand::TXT { text, split_text } => {
-                qb = qb.add_param("text", text);
-                if *split_text {
-                    qb = qb.add_param("splitText", "true");
-                }
-            }
-            RecordTypeCommand::MX { exchange, preference } => {
-                qb = qb.add_param("exchange", exchange);
-                if let Some(pref) = preference {
-                    qb = qb.add_param("preference", &pref.to_string());
-                }
-            }
-            RecordTypeCommand::NS { name_server, glue } => {
-                qb = qb.add_param("nameServer", name_server);
-                if let Some(glue_addr) = glue {
-                    qb = qb.add_param("glue", glue_addr);
-                }
-            }
-            RecordTypeCommand::PTR { ptr_name } => {
-                qb = qb.add_param("ptrName", ptr_name);
-            }
-            _ => { /* Other record types can be handled here */ }
-        }
-
-        let url = format!("{host}/api/zones/records/add?{}", qb.build());
+        let url = format!("{host}/api/zones/records/add?{}", query);
 
         let body = match app_config.tdns_client.get_body(&url).await {
             Ok(b) => b,
@@ -202,6 +149,80 @@ impl RecordTypeCommand {
         }
 
         Ok(())
+    }
+
+    fn make_query_params(
+        &self,
+        cfg: &config::Config,
+        domain_name: &str,
+        zone: &str,
+        overwrite: bool,
+        comments: Option<String>,
+        ttl: Option<u32>,
+        expiry_ttl: Option<u32>,
+    ) -> String {
+        let mut qb = QueryBuilder::new()
+            .add_param("token", &cfg.get_token())
+            .add_param("domain", &domain_name)
+            .add_param("zone", &zone)
+            .add_param("type", &self.to_string());
+
+        if overwrite {
+            qb = qb.add_param("overwrite", "true");
+        }
+        if let Some(comments) = comments {
+            qb = qb.add_param("comments", &comments);
+        }
+        if let Some(ttl) = ttl {
+            qb = qb.add_param("ttl", &ttl.to_string());
+        }
+        if let Some(expiry_ttl) = expiry_ttl {
+            qb = qb.add_param("expiryTtl", &expiry_ttl.to_string());
+        }
+
+        let extra_params = self.make_record_type_params();
+        qb = qb.merge(extra_params);
+        qb.build()
+    }
+
+    fn make_record_type_params(&self) -> QueryBuilder {
+        match self {
+            RecordTypeCommand::A { address, ptr, ptr_zone, update_svcb_hints } => {
+                make_address_params(address, *ptr, *ptr_zone, *update_svcb_hints)
+            }
+            RecordTypeCommand::AAAA { address, ptr, ptr_zone, update_svcb_hints } => {
+                make_address_params(address, *ptr, *ptr_zone, *update_svcb_hints)
+            }
+            RecordTypeCommand::CNAME { cname } => QueryBuilder::new().add_param("cname", cname),
+            RecordTypeCommand::TXT { text, split_text } => {
+                let mut qb = QueryBuilder::new().add_param("text", text);
+                if *split_text {
+                    qb = qb.add_param("splitText", "true");
+                }
+                qb
+            }
+            RecordTypeCommand::MX { exchange, preference } => {
+                let mut qb = QueryBuilder::new().add_param("exchange", exchange);
+                if let Some(pref) = preference {
+                    qb = qb.add_param("preference", &pref.to_string());
+                }
+                qb
+            }
+            RecordTypeCommand::NS { name_server, glue } => {
+                let mut qb = QueryBuilder::new().add_param("nameServer", name_server);
+                if let Some(glue_addr) = glue {
+                    qb = qb.add_param("glue", glue_addr);
+                }
+                qb
+            }
+            RecordTypeCommand::PTR { ptr_name } => {
+                QueryBuilder::new().add_param("ptrName", ptr_name)
+            }
+            _ => {
+                /* Other record types can be handled here */
+                QueryBuilder::new()
+            }
+        }
     }
 }
 
@@ -274,5 +295,41 @@ mod tests {
                 expected
             );
         }
+    }
+
+    #[test]
+    fn make_query_params_test() {
+        let cfg = config::Config::new("host1", "token1");
+        let zone = "example.com";
+
+        let cases = vec![
+            (
+                "www",
+                RecordTypeCommand::CNAME { cname: "cname.example.com".to_string() },
+                "cname=cname.example.com&domain=www.example.com&token=token1&type=CNAME&zone=example.com",
+            ),
+            (
+                "",
+                RecordTypeCommand::TXT { text: "some text".to_string(), split_text: false },
+                "domain=example.com&token=token1&text=some text&type=TXT&zone=example.com",
+            ),
+        ];
+        for (domain, record_cmd, expected) in cases {
+            let domain_name = make_domain_name(domain, zone);
+            let query =
+                record_cmd.make_query_params(&cfg, &domain_name, zone, false, None, None, None);
+            assert_eq!(query, expected);
+        }
+
+        //     fn make_query_params(
+        //     &self,
+        //     cfg: &config::Config,
+        //     domain_name: &str,
+        //     zone: &str,
+        //     overwrite: bool,
+        //     comments: Option<String>,
+        //     ttl: Option<u32>,
+        //     expiry_ttl: Option<u32>,
+        // ) -> String {
     }
 }
