a2eff018418e843d11bb9d671c26f99eeb195c78
Sean Laurent <organicveggie@gmail.com>
2026-03-14 13:36:58 -0700

    Refactor update_record tests

--- tests/update_record_test.rs
+++ tests/update_record_test.rs
@@ -3,13 +3,25 @@ use serde::{Deserialize, Serialize};
 use std::{io::Cursor, path::PathBuf, rc::Rc};
 use tdns::{config, run_cli, zone};
 
-const TOKEN: &str = "test-add-token";
-
 fn make_mock_config(url: String, token: String) -> config::MockConfigManager {
     let mut mock = config::MockConfigManager::new();
     mock.expect_read_config_file()
         .returning(move |_| Ok(config::Config::from_strings(url.clone(), token.clone())));
     mock
+}
+
+fn make_response_filename(testcase_path: &PathBuf) -> PathBuf {
+    // Build the mock response filename from the test fixture name
+    let mut response_path = testcase_path.with_extension("");
+    let mock_response_base_filename = match response_path.file_name() {
+        Some(name) => name.to_os_string(),
+        None => panic!("Failed to extract filename from {}", response_path.display()),
+    };
+    response_path.pop();
+    response_path.push("response");
+    response_path.push(mock_response_base_filename);
+    response_path.add_extension("json");
+    response_path
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -27,6 +39,8 @@ struct UpdateRecordTestCase {
 #[rstest]
 #[tokio::test]
 async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.toml")] testcase_path: PathBuf) {
+    const TOKEN: &str = "test-add-token";
+
     let file_content = std::fs::read_to_string(testcase_path.as_path()).unwrap();
     let test_case: UpdateRecordTestCase = match toml::from_str(&file_content) {
         Ok(tc) => tc,
@@ -34,15 +48,7 @@ async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.to
     };
 
     // Build the mock response filename from the test fixture name
-    let mut response_path = testcase_path.with_extension("");
-    let mock_response_base_filename = match response_path.file_name() {
-        Some(name) => name.to_os_string(),
-        None => panic!("Failed to extract filename from {}", response_path.display()),
-    };
-    response_path.pop();
-    response_path.push("response");
-    response_path.push(mock_response_base_filename);
-    response_path.add_extension("json");
+    let response_path = make_response_filename(&testcase_path);
 
     // Load the mock response
     let file_content = std::fs::read_to_string(response_path.as_path()).unwrap();
@@ -94,4 +100,21 @@ async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.to
     run_cli(&mut app_config, "test-config.json", &cli_command).await;
 
     mock.assert();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[rstest]
+    #[case("test/1.toml", "test/response/1.json")]
+    #[case("test/subdir/2.toml", "test/subdir/response/2.json")]
+    #[case("test/s1/s2/3.toml", "test/s1/s2/response/3.json")]
+    #[case("4.toml", "response/4.json")]
+    #[case("/abs/path/5.toml", "/abs/path/response/5.json")]
+    fn test_make_response_filename(#[case] path: String, #[case] expected: String) {
+        let pb = make_response_filename(&PathBuf::from(path));
+        let got = pb.to_str().unwrap();
+        assert_eq!(got, expected);
+    }
 }
