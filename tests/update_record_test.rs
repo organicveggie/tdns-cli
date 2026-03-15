b8d998768507a4dec241515c5c41895041f8fda0
Sean Laurent <organicveggie@gmail.com>
2026-03-14 17:25:27 -0700

    Move update_record test responses into toml fixtures

--- tests/update_record_test.rs
+++ tests/update_record_test.rs
@@ -10,20 +10,6 @@ fn make_mock_config(url: String, token: String) -> config::MockConfigManager {
     mock
 }
 
-fn make_response_filename(testcase_path: &PathBuf) -> PathBuf {
-    // Build the mock response filename from the test fixture name
-    let mut response_path = testcase_path.with_extension("");
-    let mock_response_base_filename = match response_path.file_name() {
-        Some(name) => name.to_os_string(),
-        None => panic!("Failed to extract filename from {}", response_path.display()),
-    };
-    response_path.pop();
-    response_path.push("response");
-    response_path.push(mock_response_base_filename);
-    response_path.add_extension("json");
-    response_path
-}
-
 #[derive(Debug, Deserialize, Serialize)]
 struct UpdateRecordTestCase {
     name: String,
@@ -33,28 +19,20 @@ struct UpdateRecordTestCase {
     comments: Option<String>,
     expiry_ttl: Option<u32>,
     command: zone::update::RecordTypeCommand,
-    mock_response: Option<serde_json::Value>,
+    response: String,
 }
 
 #[rstest]
 #[tokio::test]
-async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.toml")] testcase_path: PathBuf) {
+async fn test_update_record_toml(
+    #[files("tests/fixtures/zone/update/record/*.toml")] testcase_path: PathBuf,
+) {
     const TOKEN: &str = "test-add-token";
 
     let file_content = std::fs::read_to_string(testcase_path.as_path()).unwrap();
     let test_case: UpdateRecordTestCase = match toml::from_str(&file_content) {
         Ok(tc) => tc,
         Err(error) => panic!("Failed to parse test case from TOML file: {}", error),
-    };
-
-    // Build the mock response filename from the test fixture name
-    let response_path = make_response_filename(&testcase_path);
-
-    // Load the mock response
-    let file_content = std::fs::read_to_string(response_path.as_path()).unwrap();
-    let mock_response: serde_json::Value = match serde_json::from_str(&file_content) {
-        Ok(value) => value,
-        Err(error) => panic!("Unexpected error parsing mock response file {}: {}", response_path.display(), error),
     };
 
     // Request a new server from the pool
@@ -68,13 +46,12 @@ async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.to
     let client = tdns::client::TdnsHttpClient::new(true).unwrap();
 
     // Setup the mock response based on the test case
-    let body = serde_json::to_string(&mock_response).unwrap();
     let mock = server
         .mock("POST", zone::update::API_UPDATE_RECORD_PATH)
         .match_query(mockito::Matcher::Any)
         .with_status(200)
         .with_header("Content-Type", "application/json")
-        .with_body(body)
+        .with_body(test_case.response)
         .create_async()
         .await;
 
@@ -90,7 +67,7 @@ async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.to
     };
 
     let mut output_cursor = Cursor::new(Vec::<u8>::new());
-    let mut output = config::OutputTarget{w: &mut output_cursor};
+    let mut output = config::OutputTarget { w: &mut output_cursor };
     let mut app_config = config::ApplicationConfig {
         config_manager: Box::new(mock_cfg_mgr),
         tdns_client: Rc::new(client),
@@ -100,21 +77,4 @@ async fn test_update_record_toml(#[files("tests/fixtures/zone/update/record/*.to
     run_cli(&mut app_config, "test-config.json", &cli_command).await;
 
     mock.assert();
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[rstest]
-    #[case("test/1.toml", "test/response/1.json")]
-    #[case("test/subdir/2.toml", "test/subdir/response/2.json")]
-    #[case("test/s1/s2/3.toml", "test/s1/s2/response/3.json")]
-    #[case("4.toml", "response/4.json")]
-    #[case("/abs/path/5.toml", "/abs/path/response/5.json")]
-    fn test_make_response_filename(#[case] path: String, #[case] expected: String) {
-        let pb = make_response_filename(&PathBuf::from(path));
-        let got = pb.to_str().unwrap();
-        assert_eq!(got, expected);
-    }
 }
